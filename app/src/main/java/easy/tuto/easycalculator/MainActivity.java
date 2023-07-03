74bbdad9a5506926a9397a8f6df44cfac6301138
HatateAzura <116084355+HatateAzura@users.noreply.github.com>
2023-07-02 23:18:17 -0600

    full fixec

--- app/src/main/java/easy/tuto/easycalculator/MainActivity.java
+++ app/src/main/java/easy/tuto/easycalculator/MainActivity.java
@@ -1,112 +1,140 @@
 package easy.tuto.easycalculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.android.material.button.MaterialButton;
+
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.Scriptable;
 
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    TextView resultTv,solutionTv;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTv, solutionTv;
     MaterialButton buttonC;
-    MaterialButton buttonDivide,buttonMultiply,buttonPlus,buttonMinus,buttonEquals;
-    MaterialButton button0,button1,button2,button3,button4,button5,button6,button7,button8,button9;
-    MaterialButton buttonAC,buttonDot;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
+    boolean operatorClicked = false;
+    boolean dotClicked = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-       resultTv = findViewById(R.id.result_tv);
-       solutionTv = findViewById(R.id.solution_tv);
 
-       assignId(buttonC,R.id.button_c);
-       assignId(buttonDivide,R.id.button_divide);
-       assignId(buttonMultiply,R.id.button_multiply);
-       assignId(buttonPlus,R.id.button_plus);
-       assignId(buttonMinus,R.id.button_minus);
-       assignId(buttonEquals,R.id.button_equals);
-       assignId(button0,R.id.button_0);
-       assignId(button1,R.id.button_1);
-       assignId(button2,R.id.button_2);
-       assignId(button3,R.id.button_3);
-       assignId(button4,R.id.button_4);
-       assignId(button5,R.id.button_5);
-       assignId(button6,R.id.button_6);
-       assignId(button7,R.id.button_7);
-       assignId(button8,R.id.button_8);
-       assignId(button9,R.id.button_9);
-       assignId(buttonAC,R.id.button_ac);
-       assignId(buttonDot,R.id.button_dot);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
 
-
-
-
-
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_dot);
     }
 
-    void assignId(MaterialButton btn,int id){
+    void assignId(MaterialButton btn, int id) {
         btn = findViewById(id);
         btn.setOnClickListener(this);
     }
 
-
     @Override
     public void onClick(View view) {
-        MaterialButton button =(MaterialButton) view;
+        MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
         String dataToCalculate = solutionTv.getText().toString();
 
-        if(buttonText.equals("AC")){
-            solutionTv.setText("");
-            resultTv.setText("0");
-            return;
-        }
-        if(buttonText.equals("=")){
-            solutionTv.setText(resultTv.getText());
-            return;
-        }
-        if(buttonText.equals("C")){
-            if(dataToCalculate.length()>0) {
-                dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
-            }else{
+        switch (buttonText) {
+            case "AC":
                 solutionTv.setText("");
                 resultTv.setText("0");
+                operatorClicked = false;
+                dotClicked = false;
                 return;
-            }
-        }else{
-            dataToCalculate = dataToCalculate+buttonText;
+            case "=":
+                solutionTv.setText(resultTv.getText());
+                break;
+            case "C":
+                if (dataToCalculate.isEmpty()) {
+                    solutionTv.setText("");
+                    resultTv.setText("0");
+                    return;
+                } else {
+                    if (dataToCalculate.length() == 1) {
+                        solutionTv.setText("");
+                        resultTv.setText("0");
+                        break;
+                    }else{
+                        dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+                    }
+                }
+                solutionTv.setText(dataToCalculate);
+                break;
+            case ".":
+                if (!dotClicked) {
+                    dataToCalculate += buttonText;
+                    solutionTv.setText(dataToCalculate);
+                    dotClicked = true;
+                }
+                break;
+            case "+":
+            case "-":
+            case "*":
+            case "/":
+                if (!operatorClicked) {
+                    dataToCalculate += buttonText;
+                    solutionTv.setText(dataToCalculate);
+                    operatorClicked = true;
+                    dotClicked = false;
+                }
+                break;
+            default:
+                dataToCalculate += buttonText;
+                solutionTv.setText(dataToCalculate);
+                operatorClicked = false;
+                break;
         }
-        solutionTv.setText(dataToCalculate);
 
         String finalResult = getResult(dataToCalculate);
 
-        if(!finalResult.equals("Err")){
+        if (!finalResult.equals("Err")) {
             resultTv.setText(finalResult);
         }
-
     }
 
-    String getResult(String data){
-        try{
-            Context context  = Context.enter();
+    String getResult(String data) {
+        try {
+            Context context = Context.enter();
             context.setOptimizationLevel(-1);
             Scriptable scriptable = context.initStandardObjects();
-            String finalResult =  context.evaluateString(scriptable,data,"Javascript",1,null).toString();
-            if(finalResult.endsWith(".0")){
-                finalResult = finalResult.replace(".0","");
+            String finalResult = context.evaluateString(scriptable, data, "Javascript", 1, null).toString();
+            if (finalResult.endsWith(".0")) {
+                finalResult = finalResult.replace(".0", "");
             }
             return finalResult;
-        }catch (Exception e){
+        } catch (Exception e) {
             return "Err";
         }
     }
-
 }
+
+
